61ad74eaf90f9d476789f905cbf24d7039a427df
mister-mohit <93255269+mister-mohit@users.noreply.github.com>
2023-07-15 13:16:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomImage = "./images/dice" + randomNumber1 + ".png";
+var randomImage = "./dice" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -10,7 +10,7 @@ image1.setAttribute("src", randomImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImage2 = "./images/dice" + randomNumber2 + ".png" ;
+var randomImage2 = "./dice" + randomNumber2 + ".png" ;
 
 var image2= document.querySelector(".img2");
 
